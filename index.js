46a863d1b99c2472ffef21368e226a3102a5c8ec
Freddie Bergener <57042252+f-bergener@users.noreply.github.com>
2025-01-09 00:11:04 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const updateAttributeForElementArray = (elementArray, attribute, newValue) => {
+  for (const element of elementArray) {
+    // Add attribute check to avoid making unnecessary changes
+    if (element.getAttribute(attribute) !== newValue) {
+      element.setAttribute(attribute, newValue);
+    }
+  }
+};
+
 if (window.location.href.match('^https?://areg.appfolio.com/listings/?$')) {
   // The following code is used for updating areg.appfolio.com/listings
 
@@ -5,34 +14,34 @@ if (window.location.href.match('^https?://areg.appfolio.com/listings/?$')) {
   const topBarTitle = document.getElementsByClassName('js-topbar-title')[0];
   topBarTitle.insertAdjacentText('afterend', 'Freddie Bergener');
 
-  // Swap the links for the “apply now” buttons to https://www.findigs.com/
-  const applyNowButtons = document.getElementsByClassName('js-listing-apply');
+  // This function updates each listing that is currently on the page
+  const updateListingsPageContent = () => {
+    // Swap the links for the “apply now” buttons to https://www.findigs.com/
+    const applyNowButtons = document.getElementsByClassName('js-listing-apply');
 
-  for (const button of applyNowButtons) {
-    button.setAttribute('href', 'https://www.findigs.com/');
-  }
+    updateAttributeForElementArray(
+      applyNowButtons,
+      'href',
+      'https://www.findigs.com/'
+    );
 
-  // Swap the photos for https://placecats.com
-
-  // This function updates each listing image that is currently on the page
-  const updateEachListingImage = () => {
+    // Swap the photos for https://placecats.com
     const listingImages = document.getElementsByClassName(
       'listing-item__image'
     );
 
-    for (const image of listingImages) {
-      // Only update new images to hopefully speed up the process
-      if (image.getAttribute('src') !== 'https://placecats.com/300/200') {
-        image.setAttribute('src', 'https://placecats.com/300/200');
-      }
-    }
+    updateAttributeForElementArray(
+      listingImages,
+      'src',
+      'https://placecats.com/300/200'
+    );
   };
 
   // This first invocation is needed for the first batch of listings
-  updateEachListingImage();
+  updateListingsPageContent();
 
   // This observer is needed for new listings that appear as the user scrolls
-  const mutationObserver = new MutationObserver(updateEachListingImage);
+  const mutationObserver = new MutationObserver(updateListingsPageContent);
 
   const resultContainer = document.getElementById('result_container');
   mutationObserver.observe(resultContainer, {
@@ -50,38 +59,34 @@ if (window.location.href.match('^https?://areg.appfolio.com/listings/?$')) {
 
   applyNowButton[0].setAttribute('href', 'https://www.findigs.com/');
 
-  const updateListingPageImages = () => {
+  // This function updates the images on the page
+  const updateIndividualListingPageContent = () => {
     // Update the images at the top of the listing page
     const smallImages = document.getElementsByClassName('gallery__small-image');
     const largeImages = document.getElementsByClassName('gallery__large-image');
 
-    for (const image of smallImages) {
-      if (image.getAttribute('src') !== 'https://placecats.com/300/200') {
-        image.setAttribute('src', 'https://placecats.com/300/200');
-      }
-    }
-
-    for (const image of largeImages) {
-      if (image.getAttribute('src') !== 'https://placecats.com/300/200') {
-        image.setAttribute('src', 'https://placecats.com/300/200');
-      }
-    }
+    updateAttributeForElementArray(
+      [...smallImages, ...largeImages],
+      'src',
+      'https://placecats.com/300/200'
+    );
 
     // Update the images so the cat images appear when you click into them
     const swipeBoxAnchors = document.getElementsByClassName('swipebox');
-
-    for (const anchor of swipeBoxAnchors) {
-      if (anchor.getAttribute('href', 'https://placecats.com/300/200')) {
-        anchor.setAttribute('href', 'https://placecats.com/300/200');
-      }
-    }
+    updateAttributeForElementArray(
+      swipeBoxAnchors,
+      'href',
+      'https://placecats.com/300/200'
+    );
   };
 
   // This first invocation is for the images that are included in the inital rendering
-  updateListingPageImages();
+  updateIndividualListingPageContent();
 
   // This mutation observer was added to make the page update anything loaded after the inital rendering
-  const mutationObserver = new MutationObserver(updateListingPageImages);
+  const mutationObserver = new MutationObserver(
+    updateIndividualListingPageContent
+  );
   const gallery = document.getElementsByClassName('gallery')[0];
   mutationObserver.observe(gallery, {
     childList: true,
